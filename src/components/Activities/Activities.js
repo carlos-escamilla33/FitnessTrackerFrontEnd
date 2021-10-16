f66426b2e66d9c87b91792ef5ac34675ea580310
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-10-16 00:27:11 -0700

    styled activities page

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,6 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { callApi } from "../../apiFunc";
 import "./Activities.css";
+import {
+    styled,
+    Box,
+    Paper,
+    Grid
+} from "@material-ui/core";
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 const Activites = () => {
     const [activities, setActivities] = useState([]);
@@ -29,10 +42,20 @@ const Activites = () => {
             {
                 activities.map(activity => {
                     return (
-                        <div key={activity.id}>
-                            <h3>{activity.name}</h3>
-                            <p>{activity.description}</p>
-                        </div>
+                        <Box sx={{ flexGrow: 1 }} key={activity.id}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={6} md={8}>
+                                    <Item>
+                                        <div>
+                                            <h3> {activity.name}</h3>
+                                        </div>
+                                        <div>
+                                            {activity.description}
+                                        </div>
+                                    </Item>
+                                </Grid>
+                            </Grid>
+                        </Box>
                     )
                 })
             }
