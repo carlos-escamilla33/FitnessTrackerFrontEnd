df2c2853be184c98e726655c33921885436e1258
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-10-17 18:17:42 -0700

    finished delete route

--- src/components/RoutineActions/RoutineActions.js
+++ src/components/RoutineActions/RoutineActions.js
@@ -1,6 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { callApi } from "../../apiFunc";
 import { UserContext } from "../../context/UserContext";
+import {
+    Container,
+    Typography,
+    TextField,
+    Grid,
+    Card,
+    CardHeader,
+    CardContent,
+    Button
+} from "@material-ui/core";
 
 const RoutineActions = () => {
     // goal is to make a new form route and then update it down below
@@ -10,11 +20,14 @@ const RoutineActions = () => {
         goal,
         setGoal,
         isPublic,
-        setIsPublic
+        setIsPublic,
+        routines,
+        token,
+        publicRoutines,
+        user
     } = useContext(UserContext)
-    const { routineId } = useParms();
 
-    const updateRoutine = async () => {
+    const updateRoutine = async (routineId) => {
         try {
             const response = await callApi({
                 url: `/routines/${routineId}`,
@@ -34,7 +47,87 @@ const RoutineActions = () => {
         }
 
     }
+
+    const deleteRoutine = async (routineId) => {
+        try {
+            const response = await callApi({
+                url: `/routines/${routineId}`,
+                method: "DELETE",
+                token
+            })
+            await publicRoutines();
+            if (response) {
+                console.log(response);
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        publicRoutines();
+    }, []);
+
     return (
-        <></>
+        <>
+            {
+                routines.reverse().map(routine => {
+                    return (
+                        <Grid item key={routine.id} xs={12} md={6} lg={4}>
+                            <div>
+                                <Card>
+                                    <CardHeader
+                                        title={routine.name}
+                                        variant="h1"
+                                        align="center"
+                                        gutterbottom="true"
+                                    />
+                                    <CardContent align="center">
+                                        <Typography
+
+                                            variant="overline"
+                                        >
+                                            {routine.creatorName}
+                                        </Typography>
+                                        <Typography
+                                            variant="body2"
+                                            color="textSecondary"
+                                            gutterbottom="true"
+                                        >
+                                            {routine.goal}
+                                        </Typography>
+                                    </CardContent>
+                                    <Container
+                                        container="true"
+                                        justify="center"
+                                        align="center"
+                                    >
+                                        {
+                                            routine.creatorName === user && token ?
+                                                <>
+                                                    <Button
+                                                        color="inherit"
+                                                        size="small"
+                                                    >Edit</Button>
+                                                    <Button
+                                                        color="secondary"
+                                                        size="small"
+                                                        onClick={() => deleteRoutine(routine.id)}
+                                                    >Delete</Button>
+                                                </>
+                                                : null
+                                        }
+                                        <Button color="primary">View Activities</Button>
+                                    </Container>
+                                </Card>
+                            </div>
+                        </Grid>
+                    )
+                })
+            }
+        </>
     )
 }
+
+export default RoutineActions;
--- src/components/Routines/Routines.js
+++ src/components/Routines/Routines.js
@@ -1,21 +1,14 @@
 import React, { useContext, useEffect } from "react";
 import { UserContext } from "../../context/UserContext";
 import PostRoutine from "../PostRoutine/PostRoutine";
+import RoutineActions from "../RoutineActions/RoutineActions";
 import {
     Grid,
-    Container,
-    Card,
-    CardHeader,
-    CardContent,
-    Typography,
-    Button,
+    Container
 } from "@material-ui/core";
 
 const Routines = () => {
     const {
-        token,
-        user,
-        routines,
         publicRoutines,
     } = useContext(UserContext);
 
@@ -28,57 +21,7 @@ const Routines = () => {
             <Container>
                 <PostRoutine />
                 <Grid container spacing={3}>
-                    {
-                        routines.reverse().map(routine => {
-                            return (
-                                <Grid item key={routine.id} xs={12} md={6} lg={4}>
-                                    <div>
-                                        <Card>
-                                            <CardHeader
-                                                title={routine.name}
-                                                variant="h1"
-                                                align="center"
-                                                gutterbottom="true"
-                                            />
-                                            <CardContent align="center">
-                                                <Typography
-
-                                                    variant="overline"
-                                                >
-                                                    {routine.creatorName}
-                                                </Typography>
-                                                <Typography
-                                                    variant="body2"
-                                                    color="textSecondary"
-                                                    gutterbottom="true"
-                                                >
-                                                    {routine.goal}
-                                                </Typography>
-                                            </CardContent>
-                                            <Container
-                                                container="true"
-                                                justify="center"
-                                                align="center"
-                                            >
-                                                {
-                                                    routine.creatorName === user && token ?
-                                                        <>
-                                                            <Button
-                                                                color="inherit"
-                                                                onClick={() => updateRoutine(routine.id)}
-                                                            >Edit</Button>
-                                                            <Button color="secondary">Delete</Button>
-                                                        </>
-                                                        : null
-                                                }
-                                                <Button color="primary">View Activities</Button>
-                                            </Container>
-                                        </Card>
-                                    </div>
-                                </Grid>
-                            )
-                        })
-                    }
+                    <RoutineActions />
                 </Grid>
             </Container >
         </>
