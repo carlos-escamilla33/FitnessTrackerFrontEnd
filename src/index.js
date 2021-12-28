3689b33d5171bed7da25c22ea762d9e72a8c803a
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-12-27 21:32:35 -0800

    solved css padding/margin issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import { UserProvider } from './context/UserContext';
-import { MuiThemeProvider, createTheme } from '@material-ui/core';
+import { MuiThemeProvider, createTheme, CssBaseline } from '@material-ui/core';
 import App from "./App.js"
 
 const theme = createTheme({
@@ -9,13 +9,23 @@ const theme = createTheme({
             default: "#F3F3F3"
         },
     },
+    // overrides: {
+    //     MuiCssBaseline: {
+    //       '@global': {
+    //         html: {
+    //           WebkitFontSmoothing: 'auto',
+    //         },
+    //       },
+    //     },
+    //   },
 });
 
 ReactDOM.render(
-        <MuiThemeProvider theme={theme}>
-            <UserProvider>
-                <App />
-            </UserProvider>
-        </MuiThemeProvider>,
+    <MuiThemeProvider theme={theme}>
+        <UserProvider>
+            <CssBaseline />
+            <App />
+        </UserProvider>
+    </MuiThemeProvider>,
     document.getElementById('app'),
 );
